b9f7fd594fbac3abdac17ba90e304e92e4d7fa6a
Qronix <jonhead7240@gmail.com>
2018-04-26 17:40:40 -0400

    Safety commit. Added functionality to find XML file header, data set tag, and object entry tag

--- Core/fileHandler.php
+++ Core/fileHandler.php
@@ -35,26 +35,32 @@ function validateFile($directory,$filename){
 }
 
 function outputString($stringOut){
-    echo nl2br(htmlentities($stringOut,ENT_QUOTES));
+    echo nl2br(htmlentities($stringOut,ENT_QUOTES)."\n");
 }
 
 /*Locate kroll data file and split in to more manageable files*/
 
 function splitFile($fileLocation){
-    $xmlHeader = array();
+//    $xmlHeader = array();
+//    $dataSetEndTag = "";
     //find XML header -> store to header variable
     outputString("Grabbing XML schema data.....\n");
-//    if(!empty($xmlHeader=grabXMLHeader($fileLocation))){
-//        outputString("XML schema header found.\n");
-//    }else{
-//        outputString("Could not locate XML schema header, quitting....\n");
-//    }
-    $response = readFileToTarget($fileLocation,"</xs:schema>",null);
-    foreach ($response as $value){
-        outputString($value."\n");
+    if(!empty($xmlHeader=grabXMLHeader($fileLocation))){
+        outputString("XML schema header found.\n");
+    }else{
+        outputString("Could not locate XML schema header, quitting....\n");
     }
     //identify XML data set opening tag -> store to variable
+    outputString("Dataset tag is:" .$xmlHeader[1]."\n");
+    $dataSetTag = $xmlHeader[1];
+    $dataSetEndTag = createClosingTag($dataSetTag);
+    outputString("Dataset closing tag set to: ".$dataSetEndTag."\n");
     //identify XML item entry tag -> store to variable
+    outputString("Capturing object entry tag.....\n");
+    $dataEntryTag = grabDataEntryTag($fileLocation,$xmlHeader[sizeof($xmlHeader)-1]);
+    outputString("Captured object entry tag as: ".$dataEntryTag."\n");
+    $dataEntryEndTag = createClosingTag($dataEntryTag);
+    outputString("Object closing tag set to: ".$dataEntryEndTag."\n");
     //count number of items in catalog -> store to variable
     //split file in to X number of files
     //copy XML header information to beginning of each file
@@ -65,31 +71,54 @@ function splitFile($fileLocation){
     //output error or done message
 }
 
+function getTagCountInFile($fileLocation,$targetTag){
+    
+}
+
+function createClosingTag($openingTag):string{
+    $closingTag = str_replace("<","</",$openingTag);
+    return $closingTag;
+}
 function grabXMLHeader($fileLocation):array {
-//    $responseData = array();
-    $responseData = readFileToTarget($fileLocation,"</xs:schema>");
+    $responseData = readFileToTarget($fileLocation,"</xs:schema>",null);
     return $responseData;
 }
 
-function readFileToTarget($fileLocation, $target):array {
-    outputString("file target is: ".htmlentities($target,ENT_QUOTES)."\n");
+function grabDataEntryTag($fileLocation,$endOfHeaderTarget):string{
+    $responseData = readFileToTarget($fileLocation,$endOfHeaderTarget,1);
+    return $responseData[sizeof($responseData)-1];
+}
+
+function readFileToTarget($fileLocation, $target,$offset):array {
+    outputString("Line target is: ".$target."\n");
     $fileArray = array();
 
-    $fileGen = readFileGenerator($fileLocation,$target);
+    $fileGen = readFileToTargetGenerator($fileLocation,$target,$offset);
 
     foreach ($fileGen as $value){
         array_push($fileArray,$value);
-//        outputString($value."\n");
     }
     return $fileArray;
 }
 
-function readFileGenerator($fileLocation,$target){
+function readFileToTargetGenerator($fileLocation,$target,$offset){
     $file = "../".$fileLocation;
     $handle = fopen($file,'r');
     $lastLine = "";
 
-    while(trim($lastLine)!==$target){
-        yield 'value' => $lastLine=trim(fgets($handle));
+    if(is_null($offset)||$offset===0){
+        while(trim($lastLine)!==$target){
+            yield 'value' => $lastLine=trim(fgets($handle));
+        }
+    }
+    if(!is_null($offset)&&$offset!==0){
+        while(trim($lastLine)!==$target){
+            yield 'value' => $lastLine=trim(fgets($handle));
+        }
+        if(trim($lastLine)===$target){
+            for($i=0; $i<$offset; $i++){
+                yield 'value' => $lastLine=trim(fgets($handle));
+            }
+        }
     }
 }
